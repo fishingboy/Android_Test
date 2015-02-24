d727ed080dc2636289ae3e1e636dfe1e1d063f14
leo.kuo <et282523@hotmail.com>
2015-02-24 20:54:24 +0800

    共用同一個 menu

--- app/src/main/java/com/leo_kuo/android_test/LinearView.java
+++ app/src/main/java/com/leo_kuo/android_test/LinearView.java
@@ -18,7 +18,7 @@ public class LinearView extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view2, menu);
+        getMenuInflater().inflate(R.menu.menu_blank, menu);
         return true;
     }
 
--- app/src/main/java/com/leo_kuo/android_test/MainView.java
+++ app/src/main/java/com/leo_kuo/android_test/MainView.java
@@ -3,6 +3,7 @@ package com.leo_kuo.android_test;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,21 +14,24 @@ import android.widget.Toast;
 public class MainView extends ActionBarActivity
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_blank, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -65,6 +69,7 @@ public class MainView extends ActionBarActivity
     // 顯示 Linear 測試畫面
     public void showLinear(View view)
     {
+        Log.i("Leo", "Hello world Log!");
         Intent intent = new Intent();
         intent.setClass(this, LinearView.class);
         startActivity(intent);
